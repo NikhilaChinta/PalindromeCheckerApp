12ac958a9eb6b765c580745c506558947043668c
NikhilaChinta <nikhilachinta21@gmail.com>
2026-02-23 16:50:03 +0530

    UC2-added

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,14 +1,27 @@
-import java.util.Scanner;
-
-/**
- * MAIN CLASS - UseCase4PalindromeCheckerApp
- * Use Case 4: Character Array Based Validation
- */
-
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("welcome");
+        // Step 1: Hardcoded string
+        String input = "madam";
+
+        // Step 2: Assume it is palindrome
+        boolean isPalindrome = true;
+
+        // Step 3: Compare characters from both ends
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Step 4: Print result
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is NOT a Palindrome");
+        }
     }
 }
