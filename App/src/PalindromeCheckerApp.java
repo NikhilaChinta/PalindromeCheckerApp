02290cba3a72d268e1cce0e66f98cbadd467b016
deepshikakamal-08 <deepshika283@gmail.com>
2026-02-23 15:46:05 +0530

    UC3

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -4,22 +4,25 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter a string: ");
-        String input = sc.nextLine();
+        String input = scanner.nextLine();
 
         String reversed = "";
 
-        for(int i = input.length()-1; i >= 0; i--){
+        // Reverse the string
+        for (int i = input.length() - 1; i >= 0; i--) {
             reversed = reversed + input.charAt(i);
         }
 
-        if(input.equalsIgnoreCase(reversed)){
-            System.out.println("It is a Palindrome");
+        // Compare original and reversed
+        if (input.equals(reversed)) {
+            System.out.println("It is a Palindrome.");
         } else {
-            System.out.println("Not a Palindrome");
+            System.out.println("It is NOT a Palindrome.");
         }
 
-        sc.close();
+        scanner.close();
     }
 }
